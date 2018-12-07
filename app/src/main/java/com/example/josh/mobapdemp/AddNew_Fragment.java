3d19fd91113502662e471ccf9381a4d304bdb1b0
salcy19 <amsmark2@gmail.com>
2018-12-07 19:13:37 +0800

    Added progressbar precentage for uploading to remove crashing

--- app/src/main/java/com/example/josh/mobapdemp/AddNew_Fragment.java
+++ app/src/main/java/com/example/josh/mobapdemp/AddNew_Fragment.java
@@ -1,6 +1,7 @@
 package com.example.josh.mobapdemp;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -24,12 +25,14 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.internal.Storage;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
@@ -92,7 +95,11 @@ public class AddNew_Fragment extends Fragment {
 
     public void addCr(String crName, String crLocation) {
 
-        if (!TextUtils.isEmpty(crName) || !TextUtils.isEmpty(crLocation)) {
+        if (!TextUtils.isEmpty(crName) || !TextUtils.isEmpty(crLocation) || uri != null) {
+
+            final ProgressDialog progressDialog = new ProgressDialog(getActivity());
+            progressDialog.setTitle("Uploading");
+            progressDialog.show();
 
             String id = databaseCR.push().getKey();
             CrModel cr = new CrModel(id, crName, crLocation);
@@ -100,18 +107,31 @@ public class AddNew_Fragment extends Fragment {
             FirebaseUser user = firebaseAuth.getCurrentUser();
             String userID = user.getUid();
             StorageReference filepath = storageReference.child("CRPhotos").child(uri.getLastPathSegment());
-            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    Toast.makeText(getActivity(), "CR added!", Toast.LENGTH_SHORT).show();
-
-                }
-            });
+            filepath.putFile(uri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getActivity(), "CR added!", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getActivity(), exception.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                            progressDialog.setMessage("Uploaded " + ((int) progress) + "%...");
+                        }
+                    });
 
         } else {
-
             Toast.makeText(getActivity(), "Empty Inputs", Toast.LENGTH_SHORT).show();
-
         }
     }
     private void selectImage() {
